90b9d36509620dc2c351dc5daf7504dad563bd7e
mikoSL <lina.bamboo@gmail.com>
2018-05-08 14:58:44 +0300

    add codepipline, deployment can be done automatically when there is change in github

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,16 +10,29 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:184139005054:deployPortfolioTopic')
     
+    location = {
+        'bucketName': 'portfoliobuild.linafinland.com'
+        'objectKey': 'portfoliobuild.zip'
+    }
     try:
+        #codepipeline config
+        job = event.get('CodePipeline.job')
+        
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] = 'MyAppBuild':
+                    location = artifact['location']['s3Location']
+        print ('Build portfolio from' + str(location))
+        
         #sync portfolio and portfolio build buckets
         s3 = boto3.resource('s3')
         
         portfolio_bucket = s3.Bucket('portfolio.linafinland.com')
-        build_bucket = s3.Bucket('portfoliobuild.linafinland.com')
+        build_bucket = s3.Bucket(location['bucketName'])
         
         # io.BytesIO save zip file in memory instead of filesystem
         portfolio_zip = io.BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'],portfolio_zip)
         
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -32,6 +45,10 @@ def lambda_handler(event, context):
                 
         #sns content
         topic.publish(Subject='Portofolio deployed', Message='Deploy portfolio successfully!')
+        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['id'])
     except:
         topic.publish(Subject='Portofolio deploy failed', Message='Portfolio deployment failed')
         raise
