94ea1f6c83fdc9f58832e8e7dde82ff855595414
mikoSL <lina.bamboo@gmail.com>
2018-05-11 15:39:34 +0300

    update work example names

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExampleWork from './example-work';
+import ExampleWork from './example-work.js';
 
 const myWork = [
-	{
-		'title': "Example1",
-		'href': "https://example.com",
-		'desc': "no content yet, should add soon! example1. ",
-		'image': {
-			'desc': "example screenshot of a project involving code",
-			'src': "images/example1.png",
-			'comment':""
-		}
-	},
-	{
-		'title': "Example2",
-		'href': "https://example.com",
-		'desc': "no content yet, should add soon! example2",
-		'image': {
-			'desc': "example screenshot of a project involving chemistry",
-			'src': "images/example2.png",
-			'comment':""
-		}
-	},
-	{
-		'title': "Example3",
-		'href': "https://example.com",
-		'desc': "no content yet, should add soon! example3",
-		'image': {
-			'desc': "example screenshot of a project involving cat",
-			'src': "images/example3.png",
-			'comment':"pic srouce"
-		}
-	}
-]
+  {
+    'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Portfolio Boilerplate",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Work Example",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
+           https://www.flickr.com/photos/37287295@N00/2540855181`
+    }
+  }
+];
 
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
