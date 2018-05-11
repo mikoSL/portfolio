33b89e6ab955f6971e94408b2fbcf15c41f04077
mikoSL <lina.bamboo@gmail.com>
2018-05-11 10:09:21 +0300

    fix small typo bugs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+	{
+		'title': "Example1",
+		'image': {
+			'desc': "example screenshot of a project involving code",
+			'src': "images/example1.png",
+			'comment':""
+		}
+	},
+	{
+		'title': "Example2",
+		'image': {
+			'desc': "example screenshot of a project involving chemistry",
+			'src': "images/example2.png",
+			'comment':""
+		}
+	},
+	{
+		'title': "Example3",
+		'image': {
+			'desc': "example screenshot of a project involving cat",
+			'src': "images/example3.png",
+			'comment':"pic srouce"
+		}
+	}
+]
 
-console.log("webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
