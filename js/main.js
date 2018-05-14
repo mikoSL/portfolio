23b2ea379ca429caf056c3cac78ee2b931b30a2d
mikoSL <lina.bamboo@gmail.com>
2018-05-14 12:21:33 +0300

    change example names

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ import ExampleWork from './example-work.js';
 
 const myWork = [
   {
-    'title': "Work Example",
+    'title': "Work Example1",
     'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "add later.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example1.png",
@@ -14,9 +14,9 @@ const myWork = [
     }
   },
   {
-    'title': "Portfolio Boilerplate",
+    'title': "example 2",
     'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "add later2.",
     'image': {
       'desc': "A Serverless Portfolio",
       'src': "images/example2.png",
@@ -24,14 +24,13 @@ const myWork = [
     }
   },
   {
-    'title': "Work Example",
+    'title': "Work Example3",
     'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'desc': "add later 3.",
     'image': {
       'desc': "example screenshot of a project involving cats",
       'src': "images/example3.png",
-      'comment': `"Bengal cat" by roberto shabs is licensed under CC BY 2.0
-           https://www.flickr.com/photos/37287295@N00/2540855181`
+      'comment': ""
     }
   }
 ];
