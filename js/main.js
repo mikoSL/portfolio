4debbd4f65e57602101bf342ae39e871505fdd6b
mikoSL <lina.bamboo@gmail.com>
2018-05-16 14:22:27 +0300

    update work example

--- js/main.js
+++ js/main.js
@@ -4,22 +4,22 @@ import ExampleWork from './example-work.js';
 
 const myWork = [
   {
-    'title': "Work Example1",
-    'href': "https://example.com",
-    'desc': "add later.",
+    'title': "Manage AWS EC2 with Boto3",
+    'href': "https://github.com/mikoSL/AWS_snapshot_analyzer",
+    'desc': "Manage AWS EC2 instance snapshot with boto3.",
     'image': {
-      'desc': "example screenshot of a project involving code",
+      'desc': "binary code picture",
       'src': "images/example1.png",
       'comment': ""
     }
   },
   {
-    'title': "example 2",
-    'href': "https://example.com",
-    'desc': "add later2.",
+    'title': "Serverless Video Sharing Website",
+    'href': "http://serverless-videosharingapp-transcoded.s3-website-us-east-1.amazonaws.com",
+    'desc': "Serverless and event-driven video sharing website with lambda,S3, Elastic Transcoder, Google Firebase and Autho0",
     'image': {
-      'desc': "A Serverless Portfolio",
-      'src': "images/example2.png",
+      'desc': "video sharing website",
+      'src': "images/videosharingapp.png",
       'comment': ""
     }
   },
